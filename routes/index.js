7e0c4c2bbc752c65af925f94d1368ef61a1f1669
johnny c. <56122426+pearmypie@users.noreply.github.com>
2024-07-22 16:56:59 +0300

    chore: Add route for index page with basic response

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+router.route('/')
+  .get((req, res) => {
+    res.status(200).send('index.html');
+  });
+
 router.route('/offer')
   .get((req, res) => {
     res.status(200).render('offer');

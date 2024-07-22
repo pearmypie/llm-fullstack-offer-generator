8dd9689e53f50fdd35463cc2ad8d2fa0875b5bf1
johnny c. <56122426+pearmypie@users.noreply.github.com>
2024-07-22 19:17:04 +0300

    feat: Add routes for about and contact pages

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,16 @@ router.route('/requirements')
     res.status(200).render('requirements');
   });
 
+router.route('/about')
+  .get((req, res) => {
+    res.status(200).render('about');
+  });
+
+router.route('/contact')
+  .get((req, res) => {
+    res.status(200).render('contact');
+  });
+
 router.route('*')
   .get((req, res) => {
     res.status(404).render('not_found');

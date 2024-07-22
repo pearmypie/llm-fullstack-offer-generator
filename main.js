dba65d19d3a31a23c0160e005a70831656ad7f3b
johnny c. <56122426+pearmypie@users.noreply.github.com>
2024-07-22 15:45:44 +0300

    chore: Add morgan logging middleware for request logging

--- main.js
+++ main.js
@@ -2,12 +2,15 @@ require('dotenv').config();
 const port = process.env.APP_PORT || 3000;
 
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+
+app.use(morgan('common'));
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
